c2aed8a1c5eefe5d887fa25e1fc51c0e1ca1c3c3
Patrick Nelson <patnelson@ebay.com>
2022-03-14 04:21:18 -0700

    WIP: Experimental test to verify builds work by eviscerating dependency upon eleventyInstance and (currently) no file watching (due to lack of dependency).

--- index.js
+++ index.js
@@ -9,100 +9,85 @@ const _debounce = require('lodash.debounce');
 const path = require('path');
 const map = require('map-stream');
 const remap = function(file, cb) {
-    file.path = path.parse(file.path).base;
-    cb(null, file);
+  file.path = path.parse(file.path).base;
+  cb(null, file);
 };
 const PLUGIN_NAME = 'Eleventy-Plugin-Sass';
 const PLUGIN_SHORT = 'PS';
 const defaultOptions = {
-    watch: ['**/*.{scss,sass}', '!node_modules/**'],
-    sourcemaps: false,
-    cleanCSS: true,
-    cleanCSSOptions: {},
-    sassOptions: {},
-    autoprefixer: true,
-    outputDir: undefined,
-    remap: false,
-    compiler: require('sass'),
-    onError: null,
+  watch: ['**/*.{scss,sass}', '!node_modules/**'],
+  sourcemaps: false,
+  cleanCSS: true,
+  cleanCSSOptions: {},
+  sassOptions: {},
+  autoprefixer: true,
+  outputDir: undefined,
+  remap: false,
+  compiler: require('sass'),
+  onError: null,
 };
 
 function monkeypatch(cls, fn) {
-    const orig = cls.prototype[fn.name][`_${PLUGIN_SHORT}_original`] || cls.prototype[fn.name];
-    function wrapped() {
-        return fn.bind(this, orig).apply(this, arguments);
-    }
-    wrapped[`_${PLUGIN_SHORT}_original`] = orig;
+  const orig = cls.prototype[fn.name][`_${PLUGIN_SHORT}_original`] || cls.prototype[fn.name];
 
-    cls.prototype[fn.name] = wrapped;
+  function wrapped() {
+    return fn.bind(this, orig).apply(this, arguments);
+  }
+
+  wrapped[`_${PLUGIN_SHORT}_original`] = orig;
+
+  cls.prototype[fn.name] = wrapped;
 }
 
 const compileSass = _debounce(function(eleventyInstance, options) {
-    console.log(`[${chalk.red(PLUGIN_NAME)}] Compiling Sass files...`);
-    const sass = require('gulp-sass')(options.compiler);
-    vfs.src(options.watch)
-        .pipe(gulpIf(options.sourcemaps, sourcemaps.init()))
-        .pipe(sass(options.sassOptions).on('error', options.onError || sass.logError))
-        .pipe(gulpIf(options.autoprefixer, prefix()))
-        .pipe(gulpIf(options.cleanCSS, cleanCSS(options.cleanCSSOptions)))
-        .pipe(gulpIf(options.sourcemaps, sourcemaps.write('.')))
-        .pipe(gulpIf(options.remap, map(remap)))
-        .pipe(vfs.dest( (options.outputDir || eleventyInstance.outputDir), {nodir: true} ))
-        .on('end', function() {
-            console.log(`[${chalk.red(PLUGIN_NAME)}] Done compiling Sass files`);
-            eleventyInstance.eleventyServe.reload('*.css');
-        });
+  console.log(`[${chalk.red(PLUGIN_NAME)}] Compiling Sass files...`);
+  const sass = require('gulp-sass')(options.compiler);
+  vfs.src(options.watch)
+    .pipe(gulpIf(options.sourcemaps, sourcemaps.init()))
+    .pipe(sass(options.sassOptions).on('error', options.onError || sass.logError))
+    .pipe(gulpIf(options.autoprefixer, prefix()))
+    .pipe(gulpIf(options.cleanCSS, cleanCSS(options.cleanCSSOptions)))
+    .pipe(gulpIf(options.sourcemaps, sourcemaps.write('.')))
+    .pipe(gulpIf(options.remap, map(remap)))
+    //.pipe(vfs.dest((options.outputDir || eleventyInstance.outputDir), {nodir: true}))
+    .pipe(vfs.dest((options.outputDir), {nodir: true}))
+    .on('end', function() {
+      console.log(`[${chalk.red(PLUGIN_NAME)}] Done compiling Sass files`);
+      //eleventyInstance.eleventyServe.reload('*.css');
+    });
 }, 500);
 
 function initializeWatcher(eleventyInstance, options) {
-    let firstRun = true;
-    const watcher = chokidar.watch(options.watch, {
-        persistent: true
+  let firstRun = true;
+  const watcher = chokidar.watch(options.watch, {
+    persistent: true
+  });
+  watcher
+    .on('add', path => {
+      if (!firstRun) {
+      }
+      firstRun = false;
+      compileSass(eleventyInstance, options);
+    })
+    .on('change', path => {
+      compileSass(eleventyInstance, options);
     });
-    watcher
-        .on('add', path => {
-            if (!firstRun) {
-            }
-            firstRun = false;
-            compileSass(eleventyInstance, options);
-        })
-        .on('change', path => {
-            compileSass(eleventyInstance, options);
-        });
 }
 
 module.exports = {
-    initArguments: {},
-    configFunction: function(eleventyConfig, options) {
-        setImmediate(function() {
-            options = { ...defaultOptions, ...options };
-            let initialized = false;
-            const Eleventy = require('@11ty/eleventy/src/Eleventy.js');
-            if (Eleventy.prototype) {
-                function write(original) {
-                    if (!initialized && !this.isDryRun) {
-                        compileSass(this, options);
-                    }
-                    return original.apply(this);
-                }
-                function watch(original) {
-                    if (!initialized) {
-                        initializeWatcher(this, options);
-                        initialized = true;
-                    }
-                    return original.apply(this);
-                }
-                function serve(original, port) {
-                    if (!initialized) {
-                        initializeWatcher(this, options);
-                        initialized = true;
-                    }
-                    return original.apply(this, [port]);
-                }
-                monkeypatch(Eleventy, write);
-                monkeypatch(Eleventy, watch);
-                monkeypatch(Eleventy, serve);
-            }
-        });
-    }
+  initArguments: {},
+  configFunction: function(eleventyConfig, options) {
+
+    options = {...defaultOptions, ...options};
+
+    eleventyConfig.on('eleventy.before', async () => {
+      compileSass(null, options);
+    });
+
+    // ... changes should only be CSS, allowing eleventy to optimize for *.css only reloads.
+    eleventyConfig.addWatchTarget(options.outputDir);
+
+    // TODO: Figure out how to initialize watch properly without monkey-patching.
+
+  }
 };
